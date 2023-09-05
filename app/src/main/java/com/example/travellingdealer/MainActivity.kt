8ab0a34df19fd5ed213343cfe0221480bf8332fc
HKalay <hamdullahkalay@Hamdullah-MBP-2.home>
2023-09-05 22:56:07 +0300

    Calculated is done

--- app/src/main/java/com/example/travellingdealer/MainActivity.kt
+++ app/src/main/java/com/example/travellingdealer/MainActivity.kt
@@ -1,33 +1,83 @@
 package com.example.travellingdealer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.bntAddNode
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var node = Node(x = 3, y = 7)
+    private var node: Node? = null
+    private var nodeList = ArrayList<Node>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        initClickListener()
 
-    }
-
-    private fun initClickListener(){
-        bntAddNode.setOnClickListener {
-            addNode()
+        btnAddCustomerLocation.setOnClickListener {
+            addCustomerLocation()
         }
+
+        btnCalculate.setOnClickListener {
+            calculateDistance()
+        }
+
+        btnShowCalculations.setOnClickListener {
+            showAlertDialog(title = "Locations - Calculations", message = node?.printNodes())
+        }
+
+        btnLocationCount.setOnClickListener {
+            showAlertDialog(title = "Location Count", message = node?.length().toString())
+        }
+
+        btnDeleteLocation.setOnClickListener {
+            nodeList.shuffle()
+            showAlertDialog(title = "Delete Location Info",message= node?.deleteNode(x = nodeList[0].x, y = nodeList[0].y))
+        }
+
+        node = Node(3, 7)
+        nodeList.add(Node(3, 7))
+        node?.distance = 0.0
     }
 
-    private fun addNode() {
-        node.appendToEnd(x = 1, y = 4)
-        node.appendToEnd(x = 1, y = 10)
-        node.appendToEnd(x = 2, y = 1)
-        node.appendToEnd(x = 5, y = 2)
-        node.appendToEnd(x = 6, y = 5)
-        node.appendToEnd(x = 8, y = 4)
-        node.appendToEnd(x = 8, y = 9)
-        node.appendToEnd(x = 9, y = 2)
+    private fun calculateDistance() {
+        val distance = node?.sumOfNodes()
+        val stringTotalDistance = String.format("%.5f", distance?.times(2))
+        showAlertDialog(
+            title = "Total Distance",
+            message = "Distance: $stringTotalDistance"
+        )
+    }
+
+    private fun addCustomerLocation() {
+        node?.appendToEnd(1, 4)
+        node?.appendToEnd(1, 10)
+        node?.appendToEnd(2, 1)
+        node?.appendToEnd(5, 2)
+        node?.appendToEnd(6, 5)
+        node?.appendToEnd(8, 4)
+        node?.appendToEnd(8, 9)
+        node?.appendToEnd(9, 2)
+
+
+        nodeList.add(Node(1, 4))
+        nodeList.add(Node(1, 10))
+        nodeList.add(Node(2, 1))
+        nodeList.add(Node(5, 2))
+        nodeList.add(Node(6, 5))
+        nodeList.add(Node(8, 4))
+        nodeList.add(Node(8, 9))
+        nodeList.add(Node(9, 2))
+
+        showAlertDialog(title = "Message", message = "Customer locations added")
+    }
+
+    private fun showAlertDialog(message: String?, title: String?) {
+        AlertDialog.Builder(this)
+            .setMessage(message)
+            .setTitle(title)
+            .setPositiveButton("OK") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 }
--- app/src/main/java/com/example/travellingdealer/Node.kt
+++ app/src/main/java/com/example/travellingdealer/Node.kt
@@ -1,44 +1,94 @@
 package com.example.travellingdealer
 
+import kotlin.math.pow
+import kotlin.math.sqrt
+
 class Node(var x: Int, var y: Int) {
 
-    var following: Node? = null
+    var distance: Double? = null
+    private var following: Node? = null
 
     fun appendToEnd(x: Int, y: Int) {
-        val end = Node(x = x, y = y)
 
+        //Bulunduğum konumdan (x,y) - hedeflediğimiz konumu çıkartıyoruz. (x,y).
+        //Çıkan x ve y değerlerinin toplamlarının karakökü alınır. Kök dışarı çıkartılır ve mesafe bulunur.
+
+        val end = Node(x = x, y = y)
         var n: Node? = this
+
+        val xvalue = (n!!.x - x).toDouble().pow(2)
+        val yValue = (n.y - y).toDouble().pow(2)
+        end.distance = sqrt((xvalue + yValue))
 
         while (n!!.following != null) {
             n = n.following
         }
-
         n.following = end
     }
 
     // TODO:: Implement to return the length of the SinglyLinkedList
     // For example:: --> 5 --> 6 --> 7 --> 3 --> .
-    fun printNodes() {}
+    fun printNodes(): String {
 
-    // TODO:: Implement to return the length of the SinglyLinkedList
-    fun length(h: Node?): Int {
-        return 0
-    }
+        var nodesData = "Factory(3, 7)"
 
-    // TODO:: Implement to return the sum of the Nodes
-    fun sumOfNodes(): Int {
-        return 0
-    }
+        val node = this
 
-    fun deleteNode(head: Node, x: Int, y: Int): Node? {
-        val n: Node = head
-
-        if (n.x == x && n.y == y) {
-            return head.following
+        var n: Node? = node.following
+        while (n != null) {
+            val stringPath = String.format("%.2f", n.distance)
+            nodesData =
+                "$nodesData\nCustomer(${n.x}, ${n.y}) - $stringPath"
+            n = n.following
         }
-        // TODO:: Implement the proper loop in order to remove given data
+        return nodesData
+    }
 
-        //
-        return head
+    fun length(): Int {
+
+        var length = 0
+        var tempNode: Node? = this
+        while (tempNode != null) {
+            length++
+            tempNode = tempNode.following
+        }
+        return length
+    }
+
+    fun sumOfNodes(): Double {
+
+        var distance = 0.0
+
+        val node = this
+
+        var n: Node? = node
+        while (n != null) {
+            distance += n.distance!!
+            n = n.following
+        }
+        return distance
+    }
+
+    fun deleteNode(x: Int, y: Int): String {
+
+        val node = this
+
+        var tempNode: Node? = node
+        var prevNode: Node? = null
+
+        if (tempNode != null && tempNode.x == x && tempNode.y == y) {
+            return "You can't delete the factory"
+        }
+
+        while (tempNode != null && !(tempNode.x == x && tempNode.y == y)) {
+            prevNode = tempNode
+            tempNode = tempNode.following
+        }
+
+        if (tempNode == null)
+            return "($x, $y) not found"
+
+        prevNode?.following = tempNode.following
+        return "($x, $y) deleted"
     }
 }
