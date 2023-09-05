38a69543deb6b5c7b5b67ba27b5fdcdfc5b7d0bb
HKalay <hamdullahkalay@Hamdullah-MBP-2.home>
2023-09-05 20:59:04 +0300

    Refactor code

--- app/src/main/java/com/example/travellingdealer/MainActivity.kt
+++ app/src/main/java/com/example/travellingdealer/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.travellingdealer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.bntAddNode
 
 class MainActivity : AppCompatActivity() {
+
+    private var node = Node(x = 3, y = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initClickListener()
+
+    }
+
+    private fun initClickListener(){
+        bntAddNode.setOnClickListener {
+            addNode()
+        }
+    }
+
+    private fun addNode() {
+        node.appendToEnd(x = 1, y = 4)
+        node.appendToEnd(x = 1, y = 10)
+        node.appendToEnd(x = 2, y = 1)
+        node.appendToEnd(x = 5, y = 2)
+        node.appendToEnd(x = 6, y = 5)
+        node.appendToEnd(x = 8, y = 4)
+        node.appendToEnd(x = 8, y = 9)
+        node.appendToEnd(x = 9, y = 2)
     }
 }
--- app/src/main/java/com/example/travellingdealer/Node.kt
+++ app/src/main/java/com/example/travellingdealer/Node.kt
@@ -1,15 +1,18 @@
 package com.example.travellingdealer
 
-class Node(var data: Int) {
+class Node(var x: Int, var y: Int) {
 
     var following: Node? = null
 
-    fun appendToEnd(data: Int) {
-        val end = Node(data)
+    fun appendToEnd(x: Int, y: Int) {
+        val end = Node(x = x, y = y)
+
         var n: Node? = this
+
         while (n!!.following != null) {
             n = n.following
         }
+
         n.following = end
     }
 
@@ -27,10 +30,10 @@ class Node(var data: Int) {
         return 0
     }
 
-    fun deleteNode(head: Node, data: Int): Node? {
+    fun deleteNode(head: Node, x: Int, y: Int): Node? {
         val n: Node = head
 
-        if(n.data == data) {
+        if (n.x == x && n.y == y) {
             return head.following
         }
         // TODO:: Implement the proper loop in order to remove given data
